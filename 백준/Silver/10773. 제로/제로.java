7405bef5a402f3c26f8b4ade2bca172ba9107e1a
kim-nini <144877020+kim-nini@users.noreply.github.com>
2024-03-28 15:38:14 +0900

    [Silver IV] Title: 제로, Time: 864 ms, Memory: 109172 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+
+        //첫째줄 정수
+        int k = sc.nextInt();
+
+        Stack<Integer> st = new Stack<>();
+
+        for(int i =0; i< k; i++){
+            int num = sc.nextInt();
+            if( num == 0 ) {
+                st.pop();
+            } else {
+                st.push(num);
+            }
+
+        }
+
+      while (!st.isEmpty()){
+          sum += st.pop();
+      }
+
+        System.out.println(sum);
+
+    }
+}
