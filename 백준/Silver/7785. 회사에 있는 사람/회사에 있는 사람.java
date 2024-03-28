694dd2d318c6d46357d5c99fe9ed3fb99d7f3458
kim-nini <144877020+kim-nini@users.noreply.github.com>
2024-03-28 17:37:52 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 792 ms, Memory: 56072 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = bf.readLine();
+
+        StringTokenizer st = new StringTokenizer(s);
+        int num = Integer.parseInt(st.nextToken()); // 로그 갯수
+
+        TreeMap<String,String> map = new TreeMap<>(Collections.reverseOrder()); // 로그 맵 정렬
+
+        String[] tmp = new String[2]; // split 용 temp 배열
+        for(int i=0; i<num; i++){
+            tmp = bf.readLine().split(" ");
+            if(map.containsKey(tmp[0])){
+                map.replace(tmp[0],tmp[1]);
+            }else {
+                map.put(tmp[0],tmp[1]);
+            }
+        }
+
+
+        for(String key : map.keySet()){
+            if(map.get(key).equals("enter")) {
+                bw.write(key + "\n");
+            }
+        }
+
+
+        bw.flush();
+        bw.close();
+
+
+    }
+}
