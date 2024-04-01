eddaaf1f49b25d1a26dfd210ad71df5d87e89967
kim-nini <144877020+kim-nini@users.noreply.github.com>
2024-04-01 18:26:39 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 152 ms, Memory: 15992 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.java
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.java
@@ -13,44 +13,39 @@ public class Main{
         int columnCnt = 0;
         boolean[][] arr = new boolean[N][N];
 
-        for (int i=0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             row.append(br.readLine());
-            for (int j=0; j<N; j++){
+            for (int j = 0; j < N; j++) {
                 arr[i][j] = row.charAt(j) == '.';
             }
             row.setLength(0);
         }
 
-        // 다음 x 가 나올때 까지 true가 2개 이상이면 카운투++
-        for (int i=0; i < N; i++) {
-            for (int j=0; j<N; j++){
-                if(!arr[i][j]){
-                    if (j - 2 >= 0 && arr[i][j-2] && arr[i][j-1]) {
-                        rowCnt ++;
+        // X이전 두칸이 true면 count++
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                // 행 검사
+                if (!arr[i][j]) { // X를 만났을 때
+                    if (j - 2 >= 0 && arr[i][j - 2] && arr[i][j - 1]) { // X 이전 두칸이 true인지 확인
+                        rowCnt++;
                     }
-                } else if (j == N-1){
-                    if (j - 1 >= 0 && arr[i][j-1] && arr[i][j]) {
-                        rowCnt ++;
+                } else if (j == N - 1) { // X를 만나지 못하고 열이 끝났을 때
+                    if (j - 1 >= 0 && arr[i][j - 1] && arr[i][j]) {
+                        rowCnt++;
                     }
                 }
-            }
-        }
-
-        for (int i=0; i < N; i++) {
-            for (int j=0; j<N; j++){
-                if(!arr[j][i]){
-                    if(j - 2 >= 0 && arr[j-2][i] && arr[j-1][i]){
+                // 열 검사
+                if (!arr[j][i]) {
+                    if (j - 2 >= 0 && arr[j - 2][i] && arr[j - 1][i]) {
                         columnCnt++;
                     }
-                }else if (j == N-1){
-                    if (j - 1 >= 0 && arr[j-1][i] && arr[j][i]) {
+                } else if (j == N - 1) {
+                    if (j - 1 >= 0 && arr[j - 1][i] && arr[j][i]) {
                         columnCnt++;
                     }
                 }
             }
         }
-
-
 
         System.out.println(rowCnt + " " + columnCnt);
         br.close();
