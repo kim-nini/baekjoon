6c313557590684951ca6e484a758526e9d3dcde4
kim-nini <144877020+kim-nini@users.noreply.github.com>
2024-04-01 13:23:20 +0900

    [Bronze III] Title: 최댓값, Time: 148 ms, Memory: 16024 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = 0;
+        String location = "";
+        for(int i = 1; i < 10; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j =1; j<10; j++){
+                int tmp = Integer.parseInt(st.nextToken());
+                if (tmp>=max){
+                    max = tmp;
+                    location = i +" "+ j ;
+                }
+            }
+        }
+
+        br.close();
+        System.out.println(max);
+        System.out.println(location);
+    }
+
+}
+
