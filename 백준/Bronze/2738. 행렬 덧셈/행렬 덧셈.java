3ff5af8172c07f79ce5a094c4a3e2ab730f6ed93
kim-nini <144877020+kim-nini@users.noreply.github.com>
2024-04-01 12:57:08 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 380 ms, Memory: 18792 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -3,31 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-
+public class Main{
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int[][] arr = new int[N][M];
 
-        for(int i = 0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j =0; j<M; j++){
-                arr[i][j] += Integer.parseInt(st.nextToken());
-            }
-        }
-        for(int i = 0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j =0; j<M; j++){
-                arr[i][j] += Integer.parseInt(st.nextToken());
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
+        matrix(st, arr, N, M, false);
+        matrix(st, arr, N, M, true);
+    }
 
-
+    static void matrix(StringTokenizer st, int[][] arr, int N, int M, boolean run) throws IOException {
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] += Integer.parseInt(st.nextToken());
+                if (run == true) System.out.print(arr[i][j] + " ");
+            }
+            if (run == true) System.out.println();
+        }
     }
 
 }
