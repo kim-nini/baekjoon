59d69d18d0c75ac6625ceee0078d3c73636d13f5
kim-nini <144877020+kim-nini@users.noreply.github.com>
2024-04-06 19:32:05 +0900

    [Silver III] Title: 통계학, Time: 672 ms, Memory: 65588 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -1,48 +1,55 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] twoFour = new int[N];
-        double one = 0;
-        Map<Integer,Integer> three = new HashMap<>();
+        int N = Integer.parseInt(br.readLine()); //수의 갯수
+
+        int[] twoFour = new int[N]; // 2) 중앙값 4) 범위
+        double one = 0; // 1) 산술평균
+        Map<Integer,Integer> map = new HashMap<>(); // 3) 최빈값
+
         for(int i =0; i<N; i++){
             int tmp = Integer.parseInt(br.readLine());
             twoFour[i] = tmp;
-            one += tmp;
-            three.put(tmp,three.getOrDefault(tmp,0)+1);
+            one += tmp; // 1) 총합
+            map.put(tmp,map.getOrDefault(tmp,0)+1); // 3) 빈도수 value++
         }
-        one =  Math.round(one/N);
-        Arrays.sort(twoFour);
 
-        int max = -4001; // 최빈값
-        for (int key: three.keySet()) {
-            if(three.get(key)>max){
-                max = three.get(key);
+        one = Math.round(one/N); // 1) 반올림
+        Arrays.sort(twoFour); // 2,4) 오름차순
+        int three = mostFrequent(map); // 3) 최빈값
+
+//        System.out.printf("%.0f\n", one); //1)
+        System.out.println((int) one); //1) 정수출력
+        System.out.println(twoFour[N/2]); //2) 중앙값
+        System.out.println(three); //3) 최빈값
+        System.out.println(twoFour[N-1]-twoFour[0]); //4) 최댓값-최솟값
+    }
+
+    static int mostFrequent(Map<Integer,Integer> map){
+        int max = -4001; // 입력 최솟값 -4000
+        for (int key: map.keySet()) {
+            if(map.get(key)>max){
+                max = map.get(key); // 제일 많이 나온 숫자의 빈도수 담기
             }
         }
-        // 여러개인지 확인
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 최빈값 담을 힙
-        for(int key: three.keySet()){
-            if(three.get(key)==max){
+        // 3) 여러개일 경우 대비
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 최빈값 담을 힙(오름차순)
+        for(int key: map.keySet()){
+            if(map.get(key)==max){ // 제일 많이 나온 숫자가 여러개일경우 전부 pq에 담음
                 pq.add(key);
             }
         }
-        int thirdAnswer = pq.poll(); // 최빈값
-        if(!pq.isEmpty()){ // 최빈값이 하나 더 있을경우 두번째로 작은 값 넣기
-            thirdAnswer = pq.poll();
+        int three = pq.poll(); // 최빈값
+        if(!pq.isEmpty()){ // 여러개 일 경우 두번 째 작은 값
+            three = pq.poll();
         }
-
-
-        System.out.printf("%.0f\n", one);
-        System.out.println(twoFour[N/2]);
-        
-        System.out.println(thirdAnswer);
-        System.out.println(twoFour[N-1]-twoFour[0]);
-
+        return three;
     }
 }
