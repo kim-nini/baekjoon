e222dabc0cba7f8eae905036e78744396c20f994
niniDev <144877020+kim-nini@users.noreply.github.com>
2024-07-08 17:53:01 +0900

    [Gold V] Title: AC, Time: 516 ms, Memory: 69000 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -3,18 +3,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
-            String functionString = br.readLine(); // 함수 RDD
+            String functionString = br.readLine(); // 함수 ex) RDD
             int n = Integer.parseInt(br.readLine()); // 원소 개수
 
-            StringBuilder sb = new StringBuilder();
             String inputArr = br.readLine();
             String[] arr = new String[0];
-            if (n > 0) {
+            if (n > 0) { // 원소 0개일 경우 처리
                 arr = inputArr.substring(1, inputArr.length() - 1).split(","); // 원소배열
             }
 
@@ -23,42 +22,43 @@ public class Main {
     }
 
     static String findPointer(String s, int n, String[] arr) {
-        int left = 0;
-        int right = n - 1;
-        int pointer = 0;
+        int left = 0; //f
+        int right = n - 1; //t
+        boolean pointer = false;
         for (int k = 0; k < s.length(); k++) {
             char function = s.charAt(k);
             if (function == 'R') {
-                pointer = pointer == 0 ? 1 : 0;
+                pointer = !pointer;
             } else if (function == 'D') {
                 if (left > right) {
                     return "error";
                 }
-                left = pointer == 0 ? left + 1 : left;
-                right = pointer == 1 ? right - 1 : right;
+                if (pointer) {
+                    right--;
+                } else {
+                    left++;
+                }
             }
         }
         return printer(left, right, pointer, arr);
     }
 
-    static String printer(int left, int right, int pointer, String[] arr) {
+    static String printer(int left, int right, boolean pointer, String[] arr) {
         StringBuilder sb = new StringBuilder();
         sb.append("[");
-        if (pointer == 0) {
-            for (int j = left; j <= right; j++) {
-                if(j==right){ sb.append(arr[j]); break;}
-                sb.append(arr[j]).append(",");
+        if (pointer) {
+            for (int j = right; j >= left; j--) {
+                sb.append(arr[j]);
+                if (j != left) sb.append(",");
             }
         } else {
-            for (int j = right; j >= left; j--) {
-                if(j==left){ sb.append(arr[j]); break;}
-                sb.append(arr[j]).append(",");
+            for (int j = left; j <= right; j++) {
+                sb.append(arr[j]);
+                if(j!=right) sb.append(",");
             }
         }
-
         sb.append("]");
         return sb.toString();
-
     }
 }
 
