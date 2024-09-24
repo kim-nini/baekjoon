8833432ef52612007b72f0d5425669d0190cb88f
niniDev <144877020+kim-nini@users.noreply.github.com>
2024-09-24 23:16:20 +0900

    [Gold II] Title: 보석 도둑, Time: 1692 ms, Memory: 116512 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,71 +1,54 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-       public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        // 보석의 개수 N, 가방의 개수 K 입력
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-
-        // 보석 배열 (무게, 가격)
         Jewel[] jewels = new Jewel[N];
         for (int i = 0; i < N; i++) {
-            int M = sc.nextInt(); // 보석의 무게
-            int V = sc.nextInt(); // 보석의 가격
-            jewels[i] = new Jewel(M, V);
+            st = new StringTokenizer(br.readLine());
+            jewels[i] = new Jewel(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
         }
-
-        // 가방 배열 (최대 무게)
-        int[] bags = new int[K];
-        for (int i = 0; i < K; i++) {
-            bags[i] = sc.nextInt();
-        }
-
-        // 보석을 무게 순으로 정렬
         Arrays.sort(jewels, Comparator.comparingInt(j -> j.weight));
 
-        // 가방을 무게 순으로 정렬
+        int[] bags = new int[K];
+        for (int i = 0; i < K; i++) {
+            bags[i] = Integer.parseInt(br.readLine());
+        }
         Arrays.sort(bags);
 
-        // 우선순위 큐: 가격이 높은 순으로 보석을 저장
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
-        long maxTotalValue = 0;
-        int jewelIndex = 0;
-
-        // 가방을 하나씩 처리
-        for (int bag : bags) {
-            // 현재 가방에 넣을 수 있는 모든 보석을 pq에 넣음
-            while (jewelIndex < N && jewels[jewelIndex].weight <= bag) {
-                pq.add(jewels[jewelIndex].value);
-                jewelIndex++;
+        long totalPrice = 0;
+        int jewelIdx = 0;
+        for( int bag : bags){
+            while(jewelIdx < N && jewels[jewelIdx].weight <= bag){
+                pq.add(jewels[jewelIdx].price);
+                jewelIdx++;
             }
 
-            // pq에서 가장 비싼 보석을 가방에 넣음
-            if (!pq.isEmpty()) {
-                maxTotalValue += pq.poll();
+            if(!pq.isEmpty()){
+                totalPrice += pq.poll();
             }
         }
 
-        // 상덕이가 훔칠 수 있는 보석의 최대 가격 출력
-        System.out.println(maxTotalValue);
+        System.out.print(totalPrice);
     }
 
-    // 보석 클래스 정의
-    static class Jewel {
+    static class Jewel{
         int weight;
-        int value;
+        int price;
 
-        Jewel(int weight, int value) {
+        public Jewel(int weight, int price){
             this.weight = weight;
-            this.value = value;
+            this.price = price;
         }
+
     }
 
 }
