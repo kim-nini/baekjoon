60e74dceae7a0d90cc468a3ceab8d4c69369dd2b
niniDev <144877020+kim-nini@users.noreply.github.com>
2024-09-25 23:43:06 +0900

    [Bronze V] Title: 사분면 고르기, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+
+        int answer = x > 0 && y > 0 ? 1 : x < 0 && y > 0 ? 2 : x < 0 && y < 0 ? 3 : 4;
+
+        System.out.print(answer);
+    }
+}
