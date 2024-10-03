a7ae9fb451d39f635a52e7a7b3a8959784073658
niniDev <144877020+kim-nini@users.noreply.github.com>
2024-10-03 22:27:27 +0900

    [Gold IV] Title: 부분합, Time: 260 ms, Memory: 24004 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -5,25 +5,22 @@ import java.util.*;
 
 public class Main {
     static int answer = Integer.MAX_VALUE;
-    static int S;
+    static int N,S;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
         int[] arr= new int[N];
-
         st = new StringTokenizer(br.readLine());
         for(int i =0; i<N;i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         find(arr);
-
-
+        System.out.print(answer==Integer.MAX_VALUE?0:answer);
     }
     static void find(int[] arr){
-        int left = 0;
-        int right = 0;
+        int left = 0, right = 0;
         int tmp = arr[left];
         while(left<=right) {
             if(tmp>=S){
@@ -32,13 +29,9 @@ public class Main {
                 left++;
             }else {
                 right++;
-                if(right>=arr.length) break;
+                if (right>=N) break;
                 tmp += arr[right];
             }
         }
-
-        System.out.print(answer==Integer.MAX_VALUE?0:answer);
     }
-
-
 }
