d6f9e755ccd81178bb5ad4e6ca9f8d5708b3a0af
niniDev <144877020+kim-nini@users.noreply.github.com>
2024-10-13 13:10:19 +0900

    [Silver V] Title: 배열 합치기, Time: 1192 ms, Memory: 301536 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -7,20 +7,35 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N+M];
+        int[] arr1 = new int[N], arr2 = new int[M];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N+M; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            st = i==N-1?new StringTokenizer(br.readLine()):st;
+        for (int i = 0; i < N; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr);
         StringBuilder sb = new StringBuilder();
-        for(int num : arr){
-            sb.append(num).append(" ");
+        int p1 = 0, p2 = 0;
+        for (int i = 0; i < N+M; i++) {
+            if(p1 < N && p2 < M) {
+                if(arr1[p1] < arr2[p2]) {
+                    sb.append(arr1[p1++]).append(" ");
+                }else{
+                    sb.append(arr2[p2++]).append(" ");
+                }
+            }else if(p1 < N) {
+                sb.append(arr1[p1++]).append(" ");
+            }else{
+                sb.append(arr2[p2++]).append(" ");
+            }
         }
+
         System.out.print(sb);
+
     }
 }
 
