60b568e1f4a5f8d5103db46ece7962ef02aae891
niniDev <144877020+kim-nini@users.noreply.github.com>
2024-11-07 23:28:39 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(br.readLine()+"??!");
+    }
+}
